42e2123a7fdc398d2d4a0f8a98c2f982cbf70aaa
rizqimaksum16-coder <rizqi.maksum16@gmail.com>
2026-08-01 09:02:10 +0700

    Mari.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
   server: {
     // Paksa Vite listen di semua interface (IPv4 & IPv6)
